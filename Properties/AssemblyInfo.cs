38e15aaed35b570da1f86202c9b35ffce69ebc0d
小豆豆 <753610399@qq.com>
2018-11-11 15:49:26 +0800

    修改程序集信息

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("DKIM-Smtp-csharp")]
-[assembly: AssemblyDescription("https://github.com/xiangyuecn/DKIM-Smtp-csharp")]
+[assembly: AssemblyTitle("https://github.com/xiangyuecn/DKIM-Smtp-csharp")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DKIMSmtp.Properties")]
+[assembly: AssemblyProduct("DKIM-Smtp-csharp")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
